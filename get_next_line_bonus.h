d7e8f0ed2b55b4d8c61871444ea964553733054e
Susumu Yagi <susumu.yagi@gmail.com>
2023-05-30 18:53:25 +0900

    modify buffer struct

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 14:04:42 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/05/30 14:06:29 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/05/30 18:51:38 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,24 @@
 
 # include <stddef.h>
 
-typedef struct s_buffer	t_buffer;
-
-struct					s_buffer
+typedef struct s_buffer
 {
-	int					fd;
-	t_buffer			*next;
+	int				fd;
+	struct s_buffer	*next;
 
-	int					n;
-	char				*bufp;
-	char				buf[BUFFER_SIZE];
-};
+	int				n;
+	char			*bufp;
+	char			buf[BUFFER_SIZE];
+}					t_buffer;
 
 typedef struct s_string
 {
-	char				*str;
-	size_t				max_len;
-	size_t				len;
-}						t_string;
+	char			*str;
+	size_t			max_len;
+	size_t			len;
+}					t_string;
 
-char					*get_next_line(int fd);
-int						ft_getc(int fd);
+char				*get_next_line(int fd);
+int					ft_getc(int fd);
 
 #endif
